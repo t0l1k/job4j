fce76e4fe5e257eff6338a8e9387bc5360b1c74a
Anatoli Tjushka <t0l1k@mail.ru>
2019-07-15 22:52:37 +0300

    4.4. Максимум из трех чисел[#159885]

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,19 +3,8 @@ package ru.job4j.condition;
 public class MultiMax {
     public int max(int first, int second, int third) {
         int result = 0;
-        result = (first > result) ? first : result;
-        result = (second > result) ? second : result;
+        result = (first > second) ? first : second;
         result = (third > result) ? third : result;
-//        if (first > result){
-//            result = first;
-//        }
-//        if (second > result){
-//            result = second;
-//        }
-//        if (third > result){
-//            result = third;
-//        }
-//        return result;
         return result;
     }
 }
