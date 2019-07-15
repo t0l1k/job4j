9694f0e79c1983805043d5d1d785593fe6ec19fc
Anatoli Tjushka <t0l1k@mail.ru>
2019-07-15 16:45:28 +0300

    2. Зависимости.[#159928]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,6 +2,10 @@ package ru.job4j.calculate;
 
 public class Calculate {
 
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello job4j");
     }
