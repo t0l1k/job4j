afe9f8506b03d69a9ee7f2a8070de41107469cbb
Anatoli Tjushka <t0l1k@mail.ru>
2019-07-15 19:36:01 +0300

    4.3. Вычисление площади треугольника[#159887]

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    static int square(int p, int k) {
+        return  0;
+    }
+
+    public static void main(String[] args) {
+        int result1 = square(4,1);
+        System.out.println(" p=4, k=1, s=1, real=%d");
+    }
+}
