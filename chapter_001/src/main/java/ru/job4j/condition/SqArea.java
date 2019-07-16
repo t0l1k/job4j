58521e3639e805b75bac562149523ee8d3f339f5
Anatoli Tjushka <t0l1k@mail.ru>
2019-07-16 18:54:25 +0300

    5.3. Построить шахматную доску в псевдографике.[#159896]

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -2,14 +2,17 @@ package ru.job4j.condition;
 
 public class SqArea {
     static int square(int p, int k) {
-
-        return  0;
+        int w = p / (2 * (1 - 1 / k));
+        int h = w / k;
+        int s = w * h;
+        return s;
     }
 
     public static void main(String[] args) {
-        int result1 = square(4,1);
-        System.out.printf(" p=4, k=1, s=1, real=%d\n",result1);
-        result1 = square(6,2);
-        System.out.printf(" p=6, k=2, s=2, real=%d",result1);
+        int result1;
+//        int result1 = square(4, 1);
+//        System.out.printf(" p=4, k=1, s=1, real=%d\n", result1);
+        result1 = square(6, 2);
+        System.out.printf(" p=6, k=2, s=2, real=%d", result1);
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Board {
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i % 2 != 0 && j%2 != 0){
+                    screen.append("X");
+                } else if (i % 2 == 0 && j % 2 == 0){
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
