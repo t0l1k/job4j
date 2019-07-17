e1971815d4baab6c9c27905dc8197f836ee014fc
Anatoli Tjushka <t0l1k@mail.ru>
2019-07-17 09:49:48 +0300

    5.3. Построить шахматную доску в псевдографике.[#159896]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -6,9 +6,7 @@ public class Board {
         String ln = System.lineSeparator();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (i % 2 != 0 && j%2 != 0){
-                    screen.append("X");
-                } else if (i % 2 == 0 && j % 2 == 0){
+                if ((i + j) % 2 == 0) {
                     screen.append("X");
                 } else {
                     screen.append(" ");
