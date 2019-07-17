c305a2ccc34d35819ff2173ee9d6d32691d1ab62
Anatoli Tjushka <t0l1k@mail.ru>
2019-07-17 09:32:27 +0300

    5.7. JavaDoc. Code convention #159895

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,12 +1,27 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate
+ *
+ * @author t0l1k
+ */
 public class Calculate {
-
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
-    }
-
+    /**
+     * Main
+     *
+     * @param args print Hello job4j
+     */
     public static void main(String[] args) {
         System.out.println("Hello job4j");
+    }
+
+    /**
+     * Method echo
+     *
+     * @param name Your name
+     * @return Echo + your name
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
