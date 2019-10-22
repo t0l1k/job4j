efc2e2f620328ba818d699649d10ff37890eff63
Anatoli Tjushka <t0l1k@mail.ru>
2019-10-22 22:57:17 +0300

    3. Изменить программу Tracker из 2-го модуля[#159975]

--- chapter_002/src/test/java/ru/job4j/start/StubTest.java
+++ chapter_002/src/test/java/ru/job4j/start/StubTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -77,7 +77,7 @@ public class StubTest {
         tracker.add(new Item("test name", "desc2"));
         Input input = new StubInput(new String[]{"5", item.getName(), "6"});
         new StartUi(input, tracker).init();
-        ArrayList<Item> result = tracker.findByName(item.getName());
+        List<Item> result = tracker.findByName(item.getName());
         assertThat(result.get(0).getName(), is(item.getName()));
     }
 }
