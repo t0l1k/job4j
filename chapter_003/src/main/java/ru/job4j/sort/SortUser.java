3301f2128f59e94c4292f4abcf428804591ae242
Anatoli Tjushka <t0l1k@mail.ru>
2019-10-23 16:01:33 +0300

    2. Сортировка User с использованием Comparator[#159961] испаравил лишний цикл

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -13,21 +13,13 @@ public class SortUser {
 
     public List<User> sortByAllFields(List<User> list) {
         Comparator<User> comp = new UserNameComparator().thenComparing(new UserAgeComparator());
-        List<User> users = new ArrayList<User>();
-        for (User u : list) {
-            users.add(u);
-        }
-        Collections.sort(users, comp);
-        return users;
+        Collections.sort(list, comp);
+        return list;
     }
 
     public List<User> sortNameLength(ArrayList<User> list) {
         Comparator<User> comp = new UserNameLenghtComparator();
-        List<User> users = new ArrayList<User>();
-        for (User u : list) {
-            users.add(u);
-        }
-        Collections.sort(users, comp);
-        return users;
+        Collections.sort(list, comp);
+        return list;
     }
 }
