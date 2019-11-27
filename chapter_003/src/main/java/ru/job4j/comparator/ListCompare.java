c8227a16fc993e1e5929c8abc0e0afa0f261926d
t0l1k <t0l1k@mail.ru>
2019-11-27 21:35:46 +0200

    подсмотрел решение в String.compareValues

--- chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -2,20 +2,13 @@ package ru.job4j.comparator;
 
 public class ListCompare {
     public int compare(String a, String b) {
-        int lowLenght = (a.length() > b.length() ? b.length() : a.length());
         char[] aArr = a.toCharArray();
         char[] bArr = b.toCharArray();
-        for (int i = 0; i < lowLenght; i++) {
-            int cmp = Integer.compare(aArr[i], bArr[i]);
-            if (cmp == -1) {
-                return -1;
-            } else if (cmp == 1) {
-                return 1;
+        for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return a.charAt(i) - b.charAt(i);
             }
         }
-        if (lowLenght == a.length() && lowLenght < b.length()) {
-            return -1;
-        }
-        return 0;
+        return a.length() - b.length();
     }
 }
